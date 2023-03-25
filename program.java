10d6fd78e334c0f5c748864dedc1990827c6b76b
VadimTalimonchik <tvadmins@gmail.com>
2023-03-25 14:09:08 +0300

    вещественный тип данных

---
+++ program.java
@@ -0,0 +1,49 @@
+/**
+ * program
+ */
+// public class program {
+//     public static void main(String[] args) {
+//         System.out.println("bye world");    
+//     }  
+// }
+
+// -----------------
+// public class program {
+//     public static void main(String[] args) {
+//         String s = null;
+//         System.out.println(s);        
+//     }    
+// }
+
+// -----------------
+// целочисленный тип данных
+// public class program {
+//     public static void main(String[] args) {
+//         short age = 10;
+//         int salary = 123456;
+//         System.out.println(age);
+//         System.out.println(salary);      
+//     }    
+// }
+
+// -----------------
+// вещественный тип данных
+// public class program {
+//     public static void main(String[] args) {
+//         float e = 2.7f;
+//         double pi = 3.1415;
+//         System.out.println(e);
+//         System.out.println(pi);      
+//     }    
+// }
+
+// -----------------
+// вещественный тип данных
+public class program {
+    public static void main(String[] args) {
+        char ch = '1';
+        System.out.println(Character.isDigit(ch));
+        char ch = '1';
+        System.out.println(Character.isDigit(ch));   
+    }    
+}
