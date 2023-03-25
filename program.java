fbcfc9e732eea32678661a62293e5dfb0aeba55f
VadimTalimonchik <tvadmins@gmail.com>
2023-03-25 18:40:39 +0300

    Массивы

--- program.java
+++ program.java
@@ -145,3 +145,46 @@ import javax.script.Invocable;
 //         System.out.println(f);
 //     }
 // }
+
+// // -----------------
+// // Массивы
+// // Одномерные массивы
+// public class program{
+//     public static void main(String[] args){
+//         int[] arr = new int[10];
+//         System.out.println(arr.length);
+
+//         arr = new int[] {1, 2, 3, 4, 5};
+//         System.out.println(arr.length);
+//         System.out.println(arr[3]); // посмотреть 3-й элемент массива
+
+//         arr[3] = 13; // записать значение для 3-го элемента
+//         System.out.println(arr[3]);
+//     }
+// }
+
+// // Многомерные массивы
+// public class program{
+//     public static void main(String[] args){
+//         int[] arr[] = new int[3][5];
+//         for(int[] line: arr) {
+//             for(int item: line) {
+//                 System.out.printf("%d", item);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// ИЛИ
+public class program{
+    public static void main(String[] args){
+        int[][] arr = new int[3][5];
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                System.out.printf("%d", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
