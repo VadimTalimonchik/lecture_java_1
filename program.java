96062d009bc3dfe3ff9bac179dd6f03eea87ebd1
VadimTalimonchik <tvadmins@gmail.com>
2023-04-01 17:41:52 +0300

    Управляющие конструкции

--- program.java
+++ program.java
@@ -379,16 +379,79 @@
 //     }
 // }
 
-public class program {
-    static int sum(int a, int b) {
-    return a+b;
-    }
-    static double factor(int n) {
-    if(n==1)return 1;
-    return n * factor(n-1);
-    }
-    public static void main(String[] args) {
-    System.out.println(sum(1, 3));
-    System.out.println(factor(5));
-    }
-}
+// public class program {
+//     static int sum(int a, int b) {
+//     return a+b;
+//     }
+//     static double factor(int n) {
+//     if(n==1)return 1;
+//     return n * factor(n-1);
+//     }
+//     public static void main(String[] args) {
+//     System.out.println(sum(1, 3));
+//     System.out.println(factor(5));
+//     }
+// }
+
+// // ---------------------------------
+// // Управляющие конструкции
+// // оператор ветвления
+// public class program {
+//     public static void main(String[] args) {
+//         int a = 1;
+//         int b = 2;
+//         int c;
+//         if (a > b) {
+//             c = a;
+//         } else {
+//             c = b;
+//         }
+//         System.out.println(c);
+//     }
+// }
+// // сокращённая форма оператора ветвления
+// public class program {
+//     public static void main(String[] args) {
+//         int a = 1;
+//         int b = 2;
+//         int c = 0;
+//         if (a > b) c = a;
+//         if (b > a) c = b;
+//         System.out.println(c);
+//     }
+// }
+
+// // тернарный оператор
+// public class program {
+//     public static void main(String[] args) {
+//         int a = 1;
+//         int b = 2;
+//         int min = a < b ? a : b;
+//         System.out.println(min);
+//     }
+// }
+
+// // оператор выбора
+// import java.util.Scanner;
+// public class program {
+// public static void main(String[] args) {
+//     int mounth = value;
+//     String text = "";
+//     switch (mounth) {
+//         case 1:
+//             text = "Autumn";
+//             break;
+//         case 2:
+//             text = "Autumns 2";
+//             break;
+//         default:
+//             text = "mistake";
+//             break;
+//         }
+//         System.out.println(text);
+//         iScanner.close();
+//     }
+// }
+
+// --------------------
+// Циклы
