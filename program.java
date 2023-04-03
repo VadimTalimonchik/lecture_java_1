0c31b2c26b5b91f41e8f8c49b34981623d5926d8
VadimTalimonchik <tvadmins@gmail.com>
2023-04-03 09:33:36 +0300

    Работа с файлами

--- program.java
+++ program.java
@@ -528,13 +528,62 @@
 //     }
 // }
 
-// цикл for : (for in или foreach)
+// // цикл for : (for in или foreach)
+// public class program {
+//     public static void main(String[] args) {
+//         int arr[] = new int[10];
+//         for(int item : arr) {
+//             System.out.printf("%d", item);
+//         }
+//         System.out.println();
+//     }
+// }
+
+// // Работа с файлами
+// // создание и запись/дозапись
+// import java.io.FileWriter;
+// import java.io.IOException;
+// public class program {
+//     public static void main(String[] args) {
+//         try (FileWriter fw = new FileWriter("file.txt", false)) {
+//             fw.write("line 1");
+//             fw.append('\n');
+//             fw.append('2');
+//             fw.append('\n');
+//             fw.write("line 3");
+//             fw.flush();
+//         } catch (IOException ex) {
+//             System.out.println(ex.getMessage());
+//         }
+//     }
+// }
+
+// // чтение посимвольно
+// import java.io.*;
+// public class program {
+//     public static void main(String[] args) throws Exception {
+//         FileReader fr = new FileReader("file.txt");
+//         int c;
+//         while ((c = fr.read()) != -1) {
+//             char ch = (char) c;
+//         if (ch == '\n') {
+//             System.out.print(ch);
+//         } else {
+//             System.out.print(ch);
+//         }
+//         }
+//     }
+// }
+
+// чтение построчно
+import java.io.*;
 public class program {
-    public static void main(String[] args) {
-        int arr[] = new int[10];
-        for(int item : arr) {
-            System.out.printf("%d", item);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+        String str;
+        while ((str = br.readLine()) != null) {
+            System.out.printf("== %s ==\n", str);
         }
-        System.out.println();
+        br.close();
     }
 }
